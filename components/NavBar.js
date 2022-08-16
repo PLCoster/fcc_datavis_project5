05f7a74b79d7c282f36524ec49ac1da45efeb9c9
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-16 13:51:20 -0700

    Created basic NavBar component for project, added to _app page

---
+++ components/NavBar.js
@@ -0,0 +1,62 @@
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+import styles from './styles/NavBar.module.css';
+
+function NavBar({ timerPhase }) {
+  const projectNames = [
+    'Bar Chart',
+    'Scatterplot Graph',
+    'Heat Map',
+    'Choropleth Map',
+    'Treemap Diagram',
+  ];
+
+  const fccProjectLinks = projectNames.map((name, index) => {
+    return (
+      <NavDropdown.Item
+        key={name}
+        href={`https://plcoster.github.io/fcc_frontendlibs_project${index}/`}
+      >
+        {name}
+      </NavDropdown.Item>
+    );
+  });
+
+  return (
+    <Navbar
+      variant="dark"
+      expand="md"
+      className={`${styles.navbar} ${
+        timerPhase === 'Session' ? styles.onSession : styles.onBreak
+      }`}
+    >
+      <Container fluid>
+        <Navbar.Brand href="#home" className={styles.navbarBrand}>
+          25 + 5 Clock
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="https://plcoster.github.io/homepage/">
+              Home
+            </Nav.Link>
+            <NavDropdown title="FreeCodeCamp Projects" id="basic-nav-dropdown">
+              {fccProjectLinks}
+            </NavDropdown>
+            <Nav.Link href="https://plcoster.github.io/homepage/projects.html">
+              All Projects
+            </Nav.Link>
+            <Nav.Link href="https://github.com/PLCoster">
+              <i className="bi bi-github"></i> Github
+            </Nav.Link>
+            <Nav.Link href="https://linkedin.com/in/plcoster">
+              <i className="bi bi-linkedin"></i> LinkedIn
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+// Import Boostrap, Bootstrap Icons and custom global styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../styles/globals.css';
+
+import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      {/* FCC TEST SCRIPT */}
+      <script
+        src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+        defer
+      />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
