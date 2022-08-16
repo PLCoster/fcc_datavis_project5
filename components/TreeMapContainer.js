eaf2bc3633b4e3e35385030f500b749786673d41
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-16 14:13:28 -0700

    Added basic TreeMapContainer component, to eventually hold TreeMap data fetching and switching between datasets

---
+++ components/TreeMapContainer.js
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap';
+
+import styles from './styles/TreeMapContainer.module.css';
+
+function TreeMapContainer() {
+  return (
+    <>
+      <Container className={styles.treemapcontainer}>
+        <p>TREEMAP CONTAINER</p>
+      </Container>
+    </>
+  );
+}
+
+export default TreeMapContainer;
