d3bd574982d4db41b98970c4cd3b2ec9515e61b1
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-16 17:01:35 -0700

    index.js now renders TreeMapContainer component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import TreeMapContainer from '../components/TreeMapContainer';
 
 import styles from '../styles/Home.module.css';
 
@@ -11,7 +12,7 @@ export default function Home() {
         <title>Treemap Diagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>TreeMap</p>
+      <TreeMapContainer />
     </>
   );
 }
