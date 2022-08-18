8b57d2580dc7941ac0ef79e1b08dad9a6f372050
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-18 10:59:30 -0700

    Removed unused stylesheet, unused code fron index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import TreeMapContainer from '../components/TreeMapContainer';
-
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
-      {/* !!! <div className={styles.container}> */}
       <Head>
         <title>Treemap Diagram</title>
         <link rel="icon" href="/favicon.ico" />
