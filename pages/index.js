23ec331679134e98cdc84aad21b03ec98b5fb635
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-18 12:58:16 -0700

    Added in base path to favicon resources for successful deployment

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 import Head from 'next/head';
 import TreeMapContainer from '../components/TreeMapContainer';
 
+const basePrefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Treemap Diagram</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={basePrefix + '/favicon.ico'} />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href={basePrefix + '/apple-touch-icon.png'}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href={basePrefix + '/favicon-32x32.png'}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href={basePrefix + '/favicon-16x16.png'}
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href={basePrefix + '/site.webmanifest'} />
       </Head>
       <TreeMapContainer />
     </>
